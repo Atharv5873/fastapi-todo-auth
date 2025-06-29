f4cce9771c09b3afeb265e21a70e096cc57a0bc1
Atharv Sharma <atharv5873@gmail.com>
2025-06-29 23:12:06 +0530

    refactor(test): move setup to utils.py and simplify test_todos

---
+++ test/utils.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from database import Base
+import pytest 
+from utils import *
+
+SQLALCHEMY_DATABASE_URL="sqlite:///./testdb.db"
+
+engine=create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread":False},
+    poolclass=StaticPool
+)
+
+TestingSessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db=TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def override_get_current_user():
+    return {'username':'atharv','id':1,'role':'admin'}  
+
+@pytest.fixture
+def test_todo():
+    todo=Todos(
+        title="Learn to code",
+        description="nil",
+        priority="5",
+        complete=False,
+        owner_id=1
+    )
+    
+    db=TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM todos;"))
+        connection.commit()
