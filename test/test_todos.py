bc8efdf9742c576e0faa746f142a9227a1bca95e
Atharv Sharma <atharv5873@gmail.com>
2025-06-29 21:38:11 +0530

    test: add unit test for POST /todo endpoint

--- test/test_todos.py
+++ test/test_todos.py
@@ -84,5 +84,22 @@ def test_read_one_authenticated_not_found():
     responce=client.get("/todo/999")
     assert responce.status_code==status.HTTP_404_NOT_FOUND
     assert responce.json()=={
-  "detail": "TODO not found"
-}
+    "detail": "TODO not found"
+    }
+    
+def test_create_todo(test_todo):
+    request_data={
+        'title':'new todo',
+        'description':'new todo',
+        'priority':5,
+        'complete':False
+    }
+
+    responce=client.post('/todo/',json=request_data)
+    assert responce.status_code == 201
+    db=TestingSessionLocal()
+    model=db.query(Todos).filter(Todos.id == 2).first()
+    assert model.title == request_data.get('title')
+    assert model.description == request_data.get('description')
+    assert model.complete == request_data.get('complete')
+    assert model.priority == request_data.get('priority')
